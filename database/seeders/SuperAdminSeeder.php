a6527c4239859eb11a9ed7af794e0b8c25794df3
saad <saada@logicodetech.com>
2026-09-17 17:20:24 +0500

    issue fix

--- database/seeders/SuperAdminSeeder.php
+++ database/seeders/SuperAdminSeeder.php
@@ -14,8 +14,8 @@ class SuperAdminSeeder extends Seeder
             ['email' => 'superadmin@example.com'],
             [
                 'name'     => 'Super Admin',
-                'email'    => 'superadmin@example.com',
-                'password' => Hash::make('Admin@12345'),
+                'email'    => 'superadmin222@gmail.com',
+                'password' => Hash::make('Saad@123'),
                 'role'     => 'super_admin',
                 'password_changed' => true, // Super admin has a proper password
                 'default_subscription_fee' => 300.00,
